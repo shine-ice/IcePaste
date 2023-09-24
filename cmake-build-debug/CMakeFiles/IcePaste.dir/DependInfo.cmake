5d0d003bc54e97bca30d37855c9bbd3aa8bac38c
icebear <2496467388@qq.com>
2023-09-24 22:49:47 +0800

    0. 项目初始化

---
+++ cmake-build-debug/CMakeFiles/IcePaste.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/CodeSpace/QtSpace/WorkSpace007/IcePaste/cmake-build-debug/IcePaste_autogen/mocs_compilation.cpp" "E:/CodeSpace/QtSpace/WorkSpace007/IcePaste/cmake-build-debug/CMakeFiles/IcePaste.dir/IcePaste_autogen/mocs_compilation.cpp.obj"
+  "E:/CodeSpace/QtSpace/WorkSpace007/IcePaste/main.cpp" "E:/CodeSpace/QtSpace/WorkSpace007/IcePaste/cmake-build-debug/CMakeFiles/IcePaste.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "IcePaste_autogen/include"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/include"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/include/QtCore"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/./mkspecs/win32-g++"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/include/QtGui"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/include/QtANGLE"
+  "D:/MyPrograms/Qt/5.15.2/mingw81_32/include/QtWidgets"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/IcePaste.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IcePaste.dir/IcePaste_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/IcePaste.dir/main.cpp.obj"
+  "CMakeFiles/IcePaste_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/IcePaste_autogen.dir/ParseCache.txt"
+  "IcePaste_autogen"
+  "IcePaste.exe"
+  "IcePaste.exe.manifest"
+  "IcePaste.pdb"
+  "libIcePaste.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IcePaste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
